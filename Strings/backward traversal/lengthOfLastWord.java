d98897d1c450bb8a70445b44e567de6c42c9bde1
shiva <shivarohithganta@gmail.com>
2026-06-25 20:09:40 +0530

    added validpalindrome and lengthoflastword string problems solutions

---
+++ Strings/backward traversal/lengthOfLastWord.java
@@ -0,0 +1,19 @@
+// two while loops don't restart from beginning so time complexity is o(n), space complexity O(1)
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int i = s.length() - 1;
+
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        int length = 0;
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+}
