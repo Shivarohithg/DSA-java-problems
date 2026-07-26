3e824009cd487c7463f3484519be4501f230822b
shiva <shivarohithganta@gmail.com>
2026-07-26 15:18:07 +0530

    added popcount codeforce solution

---
+++ sorting/popcount.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Popcount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            long n = sc.nextLong();
+            long k = sc.nextLong();
+
+            long ans = 0;
+
+            for (int bit = 0; bit < 31; bit++) {
+                long cost = 1L << bit;
+
+                long take = Math.min(k, n / cost);
+
+                ans += take;
+                n -= take * cost;
+            }
+
+            System.out.println(ans);
+        }
+
+        sc.close();
+    }
+}
