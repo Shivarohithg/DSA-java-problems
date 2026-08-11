c6591d36ab0bc24d849939a535e3ccb92629b882
shiva <shivarohithganta@gmail.com>
2026-08-11 17:10:48 +0530

    added 2093A codeforces solution

---
+++ Ideagenerator.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Ideagenerator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int k = sc.nextInt();
+
+            if (k % 2 == 1) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+
+        sc.close();
+    }
+}
