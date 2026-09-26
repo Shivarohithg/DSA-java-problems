9cf92f75adfe47c73d52e3c094ad2f53f90b1e29
shiva <shivarohithganta@gmail.com>
2026-09-26 19:10:38 +0530

    added codeforces question

---
+++ Code.java
@@ -0,0 +1,42 @@
+ import java.util.*;
+
+public class Code{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        boolean[] passed = new boolean[n + 1];
+
+        // Player X
+        int p = sc.nextInt();
+
+        for (int i = 0; i < p; i++) {
+            int level = sc.nextInt();
+            passed[level] = true;
+        }
+
+        // Player Y
+        int q = sc.nextInt();
+
+        for (int i = 0; i < q; i++) {
+            int level = sc.nextInt();
+            passed[level] = true;
+        }
+
+        // Check all levels
+        for (int i = 1; i <= n; i++) {
+            if (!passed[i]) {
+                System.out.println("Oh, my keyboard!");
+                return;
+            }
+        }
+
+        System.out.println("I become the guy.");
+
+        sc.close();
+    }
+} Code {
+    
+}
